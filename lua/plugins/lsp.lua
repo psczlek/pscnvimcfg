f6f36ada363894da4fa5e3d66f19db031301b9bc
psczlek <cyprian_ccp@proton.me>
2024-08-05 14:11:27 +0200

    change inlay hints toggle keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,7 +58,7 @@ return {
 
           -- toggle inlay hints
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>th', function()
+            map('<C-i>', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
           end
