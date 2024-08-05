4fb787fa8ad3e1f05bb4fe55534669eeae25bafa
psczlek <cyprian_ccp@proton.me>
2024-08-05 14:10:22 +0200

    add description for keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -33,25 +33,25 @@ return {
 
       local builtin = require('telescope.builtin')
       -- search help
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Search help'})
       -- search keymaps
-      vim.keymap.set('n', '<leader>fk', builtin.keymaps)
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, {desc = 'Search keymaps'})
       -- search files
-      vim.keymap.set('n', '<leader>ff', builtin.find_files)
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Search files'})
       -- search select telescope
-      vim.keymap.set('n', '<leader>ss', builtin.builtin)
+      vim.keymap.set('n', '<leader>ss', builtin.builtin, {desc = 'Search select telescope'})
       -- search current word
-      vim.keymap.set('n', '<leader>gs', builtin.grep_string)
+      vim.keymap.set('n', '<leader>gs', builtin.grep_string, {desc = 'Search current word'})
       -- grep
-      vim.keymap.set('n', '<leader>lg', builtin.live_grep)
+      vim.keymap.set('n', '<leader>lg', builtin.live_grep, {desc = 'grep'})
       -- diagnostics
-      vim.keymap.set('n', '<leader>fd', builtin.diagnostics)
+      vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {desc = 'Diagnostics'})
       -- search resume
-      vim.keymap.set('n', '<leader>fr', builtin.resume)
+      vim.keymap.set('n', '<leader>fr', builtin.resume, {desc = 'Search resume'})
       -- recent files
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles)
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, {desc = 'Recent files'})
       -- open buffers
-      vim.keymap.set('n', '<leader>fb', builtin.buffers)
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Open buffers'})
 
       vim.keymap.set('n', '<leader>/', function()
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
