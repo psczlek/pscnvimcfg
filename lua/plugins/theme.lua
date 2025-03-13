84c867cdcd968905f4cc306e773d66b2bb031b7c
psczlek <cyprian_ccp@proton.me>
2025-03-13 14:16:38 +0100

    add themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -18,8 +18,6 @@ return {
           StatusLine = {fg = '#606060'}
         },
       })
-
-      vim.cmd.colorscheme('rose-pine-moon')
     end
   },
 
@@ -27,8 +25,70 @@ return {
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
-    opts = {},
+    config = function()
+      require('tokyonight').setup({
+        transparent = true,
+        styles = {
+          sidebars = 'transparent',
+          floats = 'transparent',
+        },
+      })
+
+      vim.cmd.colorscheme('tokyonight-moon')
+    end
   },
 
-  { 'ellisonleao/gruvbox.nvim', priority = 1000 , config = true, opts = ...},
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+
+    config = function()
+      require('catppuccin').setup({
+        transparent_background = true,
+      })
+    end
+  },
+
+  {
+    'projekt0n/github-nvim-theme',
+    config = function()
+      require('github-theme').setup({
+        options = {
+          transparent = true,
+        }
+      })
+    end
+  },
+
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      require('gruvbox').setup({
+        terminal_colors = true,
+        undercurl = true,
+        underline = true,
+        bold = false,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true,
+        contrast = '',
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = true,
+      })
+    end,
+  },
 }
